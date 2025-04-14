4897e256a229e91db5bd3f9974deb1f0e369bdd2
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2025-04-14 01:33:00 +0100

    Add an example of reducing verbosity with nice facade extension methods

--- README.md
+++ README.md
@@ -14,3 +14,53 @@ A universal API for CounterStrikeSharp plugins to implement or use.
 - Multiple menus at the same time?
 - Why is there no "broadcast" API
   Complexities over navigation can be eliminated by forcing a menu to be created per player
+  It also raises questions over expected behavior. Instead encode that behavior in the plugin as desired.
+  Object pooling should be done if there are concerns over memory allocations and GC pauses
+
+
+### API a little verbose?
+
+By limiting surface area of the API, it makes it harder to abuse, and easier to amend or extend it in future.
+It is expected that consumers of this API build their own internal extension methods if they want less verbosity,
+rather than causing complexities in the interface.
+
+You can tailor your extension methods to be as concise or verbose as is appropriate for you.
+In the proof of concept sample, the `!admin` menu is set up with a very concise example.
+
+```cs
+API.CreateAndShowMenu(player, new("Admin")
+{
+	new("Players Manage")
+	{
+		new("Slap", players: Players)
+		{
+			new("0hp", exit: false),
+			new("1hp", exit: false),
+			new("10hp", exit: false),
+			new("100hp", exit: false),
+		},
+	},
+	new("Server Manage"),
+	new("Fun Commands")
+	{
+		new("God Mode", players: Players),
+		new("No Clip", players: Players),
+		new("Respawn", players: Players),
+		new("Give Weapon", players: Players)
+		{
+			new("AK47"),
+			new("M4"),
+			new("Deagle"),
+		},
+	},
+	new("Admins Manage"),
+});
+```
+
+But you could go as simple as this, if it just being a little less verbose suffices:
+
+```cs
+var menu = API.CreateMenu(player);
+menu.AddItem("Test", (menuItem) => Console.WriteLine("Test"));
+menu.Display();
+```
--- tests/ProofOfConcepts/Program.Admin.cs
+++ tests/ProofOfConcepts/Program.Admin.cs
@@ -1,10 +1,9 @@
+using System.Collections;
 using System.Collections.Generic;
 
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Entities;
 
 using CSSUniversalMenuAPI;
-using CSSUniversalMenuAPI.Extensions;
 
 namespace ProofOfConcepts;
 
@@ -12,128 +11,169 @@ public static partial class Program
 {
 	public static void AdminCommand(CCSPlayerController player)
 	{
-		var menu = API.CreateMenu(player);
-
-		menu.Title = "Admin";
-		if (menu is IMenuPriorityExtension menuPriorityExt)
-			menuPriorityExt.Priority = 100.0; // higher priority, trumps other default menus
-
+		API.CreateAndShowMenu(player, new("Admin")
 		{
-			var item = menu.CreateItem();
-			item.Title = "Players Manage";
-			item.Selected += PlayersManage_Selected;
-		}
-
-		{
-			var item = menu.CreateItem();
-			item.Title = "Server Manage";
-			item.Selected += ServersManage_Selected;
-		}
-
-		{
-			var item = menu.CreateItem();
-			item.Title = "Fun Commands";
-			item.Selected += FunCommands_Selected;
-		}
-
-		{
-			var item = menu.CreateItem();
-			item.Title = "Admins Manage";
-			item.Selected += AdminsManage_Selected;
-		}
-
-		menu.Display();
+			new("Players Manage")
+			{
+				new("Slap", players: Players)
+				{
+					new("0hp", exit: false),
+					new("1hp", exit: false),
+					new("5hp", exit: false),
+					new("10hp", exit: false),
+					new("25hp", exit: false),
+					new("50hp", exit: false),
+					new("100hp", exit: false),
+				},
+			},
+			new("Server Manage"),
+			new("Fun Commands")
+			{
+				new("God Mode", players: Players),
+				new("No Clip", players: Players),
+				new("Respawn", players: Players),
+				new("Give Weapon", players: Players)
+				{
+					new("AK47"),
+					new("M4"),
+					new("Deagle"),
+				},
+				new("Give Weapons", players: Players),
+				new("Freeze", players: Players),
+				new("Set HP", players: Players)
+				{
+					new("1"),
+					new("5"),
+					new("10"),
+					new("25"),
+					new("50"),
+					new("100"),
+					new("200"),
+					new("500"),
+					new("1000"),
+				},
+				new("Set Speed", players: Players)
+				{
+					new("10%"),
+					new("50%"),
+					new("75%"),
+					new("100%"),
+					new("150%"),
+					new("200%"),
+					new("400%"),
+				},
+				new("Set Gravity", players: Players)
+				{
+					new("10%"),
+					new("50%"),
+					new("75%"),
+					new("100%"),
+					new("150%"),
+					new("200%"),
+				},
+				new("Set Money", players: Players)
+				{
+					new("0"),
+					new("800"),
+					new("2000"),
+					new("5000"),
+					new("16000"),
+				},
+			},
+			new("Admins Manage"),
+		});
 	}
 
-	private static void PlayersManage_Selected(IMenuItem menuItem)
-	{
-		menuItem.Menu.Exit();
-	}
-	private static void ServersManage_Selected(IMenuItem menuItem)
-	{
-		menuItem.Menu.Exit();
-	}
-
-	private static void FunCommands_Selected(IMenuItem menuItem)
-	{
-		var menu = API.CreateMenu(menuItem.Menu);
-		menu.Title = "Fun Commands";
-
-		{
-			var item = menu.CreateItem();
-			item.Title = "God Mode";
-			item.Selected += FunCommand_Selected;
-		}
-		{
-			var item = menu.CreateItem();
-			item.Title = "No Clip";
-			item.Selected += FunCommand_Selected;
-		}
-		{
-			var item = menu.CreateItem();
-			item.Title = "Respawn";
-			item.Selected += FunCommand_Selected;
-		}
-		{
-			var item = menu.CreateItem();
-			item.Title = "Give Weapon";
-			item.Selected += FunCommand_Selected;
-		}
-		{
-			var item = menu.CreateItem();
-			item.Title = "Give Weapons";
-			item.Selected += FunCommand_Selected;
-		}
-		{
-			var item = menu.CreateItem();
-			item.Title = "Freeze";
-			item.Selected += FunCommand_Selected;
-		}
-		{
-			var item = menu.CreateItem();
-			item.Title = "Set HP";
-			item.Selected += FunCommand_Selected;
-		}
-		{
-			var item = menu.CreateItem();
-			item.Title = "Set Speed";
-			item.Enabled = false;
-		}
-		{
-			var item = menu.CreateItem();
-			item.Title = "Set Gravity";
-			item.Selected += FunCommand_Selected;
-			item.Enabled = false;
-		}
-		{
-			var item = menu.CreateItem();
-			item.Title = "Set Money";
-			item.Selected += FunCommand_Selected;
-			item.Enabled = false;
-		}
-
-		menu.Display();
-	}
-
-	private static void FunCommand_Selected(IMenuItem menuItem)
-	{
-		var menu = API.CreateMenu(menuItem.Menu);
-		menu.Title = menuItem.Title;
-
-		foreach (var player in Players)
-		{
-			var item = menu.CreateItem();
-			item.Title = player;
-			item.Selected += static (menuItem) => menuItem.Menu.Exit();
-		}
-
-		menu.Display();
-	}
-
-	private static void AdminsManage_Selected(IMenuItem menuItem)
-	{
-		menuItem.Menu.Exit();
-	}
 
 	private static readonly string[] Players = [ "Me", "You", "Them", "Nobody", "Everybody" ];
+}
+
+file static class Extensions
+{
+	public static void CreateAndShowMenu(this IMenuAPI api, CCSPlayerController player, MenuItems items)
+	{
+		api.CreateAndShowMenuInternal(player, items, false);
+	}
+
+	private static void CreateAndShowMenuInternal(this IMenuAPI api, CCSPlayerController player, MenuItems items, bool donePlayers)
+	{
+		var menu = api.CreateMenu(player);
+		menu.Title = items.Title;
+		menu.CreateItemsInternal(api, items, donePlayers);
+		menu.Display();
+	}
+	private static void CreateAndShowMenuInternal(this IMenuAPI api, IMenu parent, MenuItems items, bool donePlayers)
+	{
+		var menu = api.CreateMenu(parent);
+		menu.Title = items.Title;
+		menu.CreateItemsInternal(api, items, donePlayers);
+		menu.Display();
+	}
+
+	private static void CreateItemsInternal(this IMenu menu, IMenuAPI api, MenuItems items, bool donePlayers)
+	{
+		if (items.Players is not null && !donePlayers)
+		{
+			foreach (var player in items.Players)
+			{
+				var item = menu.CreateItem();
+				item.Title = player;
+
+				if (items.Items.Count == 0)
+				{
+					if (items.Exit)
+						item.Selected += (menuItem) => menuItem.Menu.Exit();
+				}
+				else
+					item.Selected += (menuItem) => CreateAndShowMenuInternal(api, menuItem.Menu, items, true);
+			}
+			return;
+		}
+
+		foreach (var childItem in items.Items)
+		{
+			var item = menu.CreateItem();
+			item.Title = childItem.Title;
+
+			if (childItem.Players is null && childItem.Items.Count == 0)
+			{
+				if (childItem.Exit)
+					item.Selected += (menuItem) => menuItem.Menu.Exit();
+			}
+			else
+				item.Selected += (menuItem) => CreateAndShowMenuInternal(api, menuItem.Menu, childItem, false);
+		}
+	}
+
+	public class MenuItems : IList<MenuItems>
+	{
+		public string Title { get; }
+		public bool Exit { get; }
+		public ItemSelectedAction? Action { get; }
+		public List<MenuItems> Items { get; } = new List<MenuItems>();
+		public IReadOnlyList<string>? Players { get; }
+
+		public MenuItems(string title, bool exit = true, ItemSelectedAction? action = null, IReadOnlyList<string>? players = null)
+		{
+			Title = title;
+			Exit = exit;
+			Action = action;
+			Players = players;
+		}
+
+
+		public MenuItems this[int index] { get => ((IList<MenuItems>)Items)[index]; set => ((IList<MenuItems>)Items)[index] = value; }
+		int ICollection<MenuItems>.Count => ((ICollection<MenuItems>)Items).Count;
+		public bool IsReadOnly => ((ICollection<MenuItems>)Items).IsReadOnly;
+		public void Add(MenuItems item) => ((ICollection<MenuItems>)Items).Add(item);
+		public void Clear() => ((ICollection<MenuItems>)Items).Clear();
+		public bool Contains(MenuItems item) => ((ICollection<MenuItems>)Items).Contains(item);
+		public void CopyTo(MenuItems[] array, int arrayIndex) => ((ICollection<MenuItems>)Items).CopyTo(array, arrayIndex);
+		public IEnumerator<MenuItems> GetEnumerator() => ((IEnumerable<MenuItems>)Items).GetEnumerator();
+		public int IndexOf(MenuItems item) => ((IList<MenuItems>)Items).IndexOf(item);
+		public void Insert(int index, MenuItems item) => ((IList<MenuItems>)Items).Insert(index, item);
+		public bool Remove(MenuItems item) => ((ICollection<MenuItems>)Items).Remove(item);
+		public void RemoveAt(int index) => ((IList<MenuItems>)Items).RemoveAt(index);
+		IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Items).GetEnumerator();
+	}
 }
