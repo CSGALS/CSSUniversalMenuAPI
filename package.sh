de33ede0f706c9cb9f1ee3264cd831b5c7eab78f
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2025-04-22 03:10:06 +0100

    Simplify packaging script

--- package.sh
+++ package.sh
@@ -1,69 +1,55 @@
 #!/bin/bash
 set -euf -o pipefail
 
-# build CSSUniversalMenuAPI.zip
-dst="./artifacts/CSSUniversalMenuAPI"
-dst_shared="$dst/addons/counterstrikesharp/shared"
-dst_plugins="$dst/addons/counterstrikesharp/plugins"
+zip_name=""
+dst=""
+dst_css=""
 
-# package CSSUniversalMenuAPI
-mkdir -p "$dst_shared/CSSUniversalMenuAPI"
-src="./src/CSSUniversalMenuAPI/bin/Release/net8.0/publish"
-cp -r "$src/." "$dst_shared/CSSUniversalMenuAPI/"
+setup_zip() {
+	zip_name="$1"
+	dst="./artifacts/$zip_name"
+	dst_css="$dst/addons/counterstrikesharp"
+}
 
-# package UniversalMenu.Compat.MenuManagerApi
-mkdir -p "$dst_plugins/UniversalMenu.Compat.MenuManagerApi"
-src="./src/UniversalMenu.Compat.MenuManagerApi/bin/Release/net8.0/publish"
-cp -r "$src/." "$dst_plugins/UniversalMenu.Compat.MenuManagerApi/"
+copy_dir() {
+	mkdir -p "$dst_css/$2/"
+	cp -r "$1/." "$dst_css/$2/"
+}
 
-# package UniversalMenu.Compat.CSSharp
-mkdir -p "$dst_plugins/UniversalMenu.Compat.CSSharp"
-src="./src/UniversalMenu.Compat.CSSharp/bin/Release/net8.0/publish"
-cp -r "$src/." "$dst_plugins/UniversalMenu.Compat.CSSharp/"
-# shared part: allows modified methods to load the 0Harmony.dll dependency
-mkdir -p "$dst_shared/0Harmony"
-mv "$dst_plugins/UniversalMenu.Compat.CSSharp/0Harmony.dll" "$dst_shared/0Harmony/"
+move_file() {
+	mkdir -p "$dst_css/$2/"
+	mv "$dst_css/$1" "$dst_css/$2/"
+}
 
-# package UniversalMenu.Compat.ScreenMenuAPI # this isn't implemented yet
-#mkdir -p "$dst_plugins/UniversalMenu.Compat.ScreenMenuAPI"
-#src="./src/UniversalMenu.Compat.ScreenMenuAPI/bin/Release/net8.0/publish"
-#cp -r "$src/." "$dst_shared/UniversalMenu.Compat.ScreenMenuAPI/"
+commit_zip() {
+	pushd "$dst"
+	7z a "../$zip_name.zip" ./
+	popd
+	rm -rf "$dst"
+}
 
-# zip CSSUniversalMenuAPI.zip
-pushd "$dst"
-7z a ../CSSUniversalMenuAPI.zip ./
-popd
-rm -rf "$dst"
+setup_zip CSSUniversalMenuAPI
+copy_dir src/CSSUniversalMenuAPI/bin/Release/net8.0/publish shared/CSSUniversalMenuAPI
+commit_zip
 
-# build UniversalMenu.Driver.ScreenMenuAPI.zip
-dst="./artifacts/UniversalMenu.Driver.ScreenMenuAPI"
-dst_shared="$dst/addons/counterstrikesharp/shared"
-dst_plugins="$dst/addons/counterstrikesharp/plugins"
+setup_zip UniversalMenu.Compat.CSSharp
+copy_dir src/UniversalMenu.Compat.CSSharp/bin/Release/net8.0/publish plugins/UniversalMenu.Compat.CSSharp
+# we move this into a shared location so that injected code can find the dll
+move_file plugins/UniversalMenu.Compat.CSSharp/0Harmony.dll shared/0Harmony
+commit_zip
 
-# package UniversalMenu.Driver.ScreenMenuAPI
-mkdir -p "$dst_plugins/UniversalMenu.Driver.ScreenMenuAPI"
-src="./src/UniversalMenu.Driver.ScreenMenuAPI/bin/Release/net8.0/publish"
-cp -r "$src/." "$dst_plugins/UniversalMenu.Driver.ScreenMenuAPI/"
+setup_zip UniversalMenu.Compat.MenuManagerApi
+copy_dir src/UniversalMenu.Compat.MenuManagerApi/bin/Release/net8.0/publish plugins/UniversalMenu.Compat.MenuManagerApi
+commit_zip
 
-# zip UniversalMenu.Driver.ScreenMenuAPI.zip
-pushd "$dst"
-7z a ../UniversalMenu.Driver.ScreenMenuAPI.zip ./
-popd
-rm -rf "$dst"
+#setup_zip UniversalMenu.Compat.ScreenMenuAPI
+#copy_dir src/UniversalMenu.Compat.ScreenMenuAPI/bin/Release/net8.0/publish plugins/UniversalMenu.Compat.ScreenMenuAPI
+#commit_zip
 
+setup_zip UniversalMenu.Driver.ScreenMenuAPI
+copy_dir src/UniversalMenu.Driver.ScreenMenuAPI/bin/Release/net8.0/publish shared/UniversalMenu.Driver.ScreenMenuAPI
+commit_zip
 
-# build UniversalMenu.Driver.MenuManagerApi.zip
-dst="./artifacts/UniversalMenu.Driver.MenuManagerApi"
-dst_shared="$dst/addons/counterstrikesharp/shared"
-dst_plugins="$dst/addons/counterstrikesharp/plugins"
-
-# package UniversalMenu.Driver.MenuManagerApi
-mkdir -p "$dst_plugins/UniversalMenu.Driver.MenuManagerApi"
-src="./src/UniversalMenu.Driver.MenuManagerApi/bin/Release/net8.0/publish"
-cp -r "$src/." "$dst_plugins/UniversalMenu.Driver.MenuManagerApi/"
-
-# zip UniversalMenu.Driver.MenuManagerApi.zip
-pushd "$dst"
-7z a ../UniversalMenu.Driver.MenuManagerApi.zip ./
-popd
-rm -rf "$dst"
+setup_zip UniversalMenu.Driver.MenuManagerApi
+copy_dir src/UniversalMenu.Driver.MenuManagerApi/bin/Release/net8.0/publish shared/UniversalMenu.Driver.MenuManagerApi
+commit_zip
